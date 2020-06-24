e13d08c33fc469d5612a8a53ed3bdb5f923bdc8f
Trikzon <trikzondev@gmail.com>
2020-06-24 16:09:04 -0400

    Fix license header

--- src/main/java/com/trikzon/shuffle/Shuffle.java
+++ src/main/java/com/trikzon/shuffle/Shuffle.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2020 Trikzon
  *
- * Flora Doubling is free software; you can redistribute it and/or modify
+ * Shuffle is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.

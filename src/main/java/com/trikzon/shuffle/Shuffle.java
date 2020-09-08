bedb5199b79b01739691ac39e254d427d9b2f29d
Trikzon <trikzondev@gmail.com>
2020-09-08 17:37:00 -0400

    Change License to MIT

--- src/main/java/com/trikzon/shuffle/Shuffle.java
+++ src/main/java/com/trikzon/shuffle/Shuffle.java
@@ -1,12 +1,3 @@
-/*
- * Copyright 2020 Trikzon
- *
- * Shuffle is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * https://www.gnu.org/licenses/
- */
 package com.trikzon.shuffle;
 
 import net.fabricmc.api.ClientModInitializer;
